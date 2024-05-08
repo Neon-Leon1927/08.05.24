6820cd71bafbbc1b36fd1ecff614b97ea52e98ab
Артем Боченин <artechenk0@gmail.com>
2024-05-08 10:21:52 +0300

    Добавьте файлы проекта.

---
+++ 08.05.24/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using System.IO;
+using System.Data;
+
+namespace _08._05._24
+{
+    public partial class Form1 : Form
+    {
+        private String Задание08.05;
+        private SQLiteConnection m_dbConn;
+        private SQLiteCommand m_sqlCmd;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
